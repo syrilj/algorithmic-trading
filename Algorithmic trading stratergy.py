f64e6271b47303c60650a7b3c63922c09645ef31
syrilj <syriltj1@gmail.com>
2022-05-04 15:39:58 -0600

    first commit

---
+++ Algorithmic trading stratergy.py
@@ -0,0 +1,55 @@
+import datetime as dt
+
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+
+plt.style.use('dark_background')
+ma_1 = 30
+ma_2 = 100
+
+start = dt.datetime.now() - dt.timedelta(days=365*3)
+end = dt.datetime.now()
+
+
+data = web.DataReader('AAPL', 'yahoo', start, end)
+data[f'SMA_{ma_1}'] = data['Adj Close'].rolling(window=ma_1).mean()
+data[f'SMA_{ma_2}'] = data['Adj Close'].rolling(window=ma_2).mean()
+
+data = data.iloc[ma_2:]
+
+plt.plot(data['Adj Close'], label="Share Price", color='lightgray')
+plt.plot(data[f'SMA_{ma_1}'], label=f'SMA{ma_1}', color='orange')
+plt.plot(data[f'SMA_{ma_2}'], label=f'SMA_{ma_2}', color='green')
+plt.legend(loc='upper left')
+plt.show()
+
+buy_signals = []
+sell_signals =[]
+trigger = 0
+
+# Creating a list of buy and sell signals.
+for x in range(len(data)):
+    if data[f'SMA_{ma_1}'].iloc[x] > data[f'SMA_{ma_2}'].iloc[x] and trigger !=1:
+        buy_signals.append(data['Adj Close'].iloc[x])
+        sell_signals.append(float('nan'))
+        trigger = 1
+    elif data[f'SMA_{ma_1}'].iloc[x] < data[f'SMA_{ma_2}'].iloc[x] and trigger != -1:
+        buy_signals.append(float('nan'))
+        sell_signals.append(data['Adj Close'].iloc[x])
+        trigger = -1
+    else:
+        buy_signals.append(float('nan'))
+        sell_signals.append(float('nan'))
+
+data['Buy Signals']= buy_signals
+data['Sell Signals']= sell_signals
+
+print(data)
+
+# Plotting the adjusted close price of the stock.
+plt.plot(data['Adj Close'], label="Share Price", alpha=0.5)
+plt.plot(data[f'SMA_{ma_1}'], label=f'SMA{ma_1}', color='orange', linestyle='--')
+plt.plot(data[f'SMA_{ma_2}'], label=f'SMA_{ma_2}', color='pink', linestyle='dashed')
+plt.scatter(data.index, data['Buy Signals'], label='Buy Signal', marker='^', color= 'green', linewidths='3')
+plt.scatter(data.index, data['Sell Signals'], label='Sell Signal', marker='^', color= 'red', linewidths='3')
+plt.legend(loc='upper left')
